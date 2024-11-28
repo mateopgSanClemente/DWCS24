57ec19e14562ea74ed36e29b3299864b1ea66f56
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-11-28 19:14:22 +0100

    Se creo un estilo externo y se añadió al fichero head.php. Se añadieron nuevas clases bootstrap al fichero menu.php, así como nuevas opciones para navegar.

---
+++ www/UD3/entregaTarea/menu.php
@@ -0,0 +1,9 @@
+<aside class="col-md-3 col-lg-2 d-md-block bg-light p-3">
+        <ul class="list-group">
+            <li class="list-group-item"><a class="nav-link" href="index.php">Página principal</a></li>
+            <li class="list-group-item"><a class="nav-link" href="nueva.php">Mis tareas</a></li>
+            <li class="list-group-item"><a class="nav-link" href="nuevaForm.php">Nueva tarea</a></li>
+            <li class="list-group-item"><a class="nav-link" href="usuarios.php">Listar usuarios</a></li>
+            <li class="list-group-item"><a class="nav-link" href="nuevoUsuarioForm.php">Nuevo usuario</a></li>
+        </ul>
+</aside>
