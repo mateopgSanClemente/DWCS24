eaa465ff8405279c6be06be8d4f692ec8579a307
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-11-29 11:35:11 +0100

    Se realizaron modificaciones en las clases bootstrap del fichero index.php.

---
+++ www/UD3/entregaTarea/index.php
@@ -0,0 +1,26 @@
+<?php include_once("head.php"); ?>
+<body>
+    <!--header-->
+    <?php
+        include_once 'header.php';
+    ?>
+    <div class="container-fluid">
+        <div class="row">
+            <!--menu-->
+            <?php
+                include 'menu.php';
+            ?>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
+                    <h2>Sobre mí</h2>
+                </div>
+                    <p>Mi nombre es <b>Mateo Pastor González</b> y estoy cursando la asignatura <b>DWCS</b>.</p>
+            </main>
+        </div>
+    </div>
+    <!--footer-->
+    <?php
+        include 'footer.php';
+    ?>
+</body>
+</html>
