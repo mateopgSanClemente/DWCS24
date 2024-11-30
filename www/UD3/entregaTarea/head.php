5e5be63e8876e6ffac1800b32d178dcd718144a9
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-11-30 01:12:51 +0100

    Implementación de cambios de estilo.

--- www/UD3/entregaTarea/head.php
+++ www/UD3/entregaTarea/head.php
@@ -6,4 +6,16 @@
     <title>UD3. Tarea</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="style.css" rel="stylesheet" type="text/css">
+    <!-- Conseguí ajustar el footer de esta forma pero me gustaría hacerlo mediante clases bootstrap -->
+    <style>
+        footer {
+            background-color: #f8f9fa;
+            text-align: center;
+            padding: 10px;
+            border-top: 1px solid #ddd;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
 </head>
