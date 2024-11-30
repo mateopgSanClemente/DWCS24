58b88eddefd3a7a96eb188b2526b9b03ac7c3653
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-11-30 01:26:07 +0100

    Se modificó el fichero menu.php para que el inicio de la base de datos y la tabla se haga desde el fichero init.php.

--- www/UD3/entregaTarea/index.php
+++ www/UD3/entregaTarea/index.php
@@ -1,26 +1,23 @@
 <?php include_once("head.php"); ?>
 <body>
     <!--header-->
-    <?php
-        include_once 'header.php';
-    ?>
-    <div class="container-fluid">
+    <?php include_once 'header.php';?>
+    <div class="container-fluid d-flex flex-column">
         <div class="row">
             <!--menu-->
             <?php
                 include 'menu.php';
             ?>
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-4 pb-2 mb-3 border-bottom">
+            <main class="col-md-9 main-content">
+                <div class="pt-4 pb-2 mb-3 border-bottom">
                     <h2>Sobre mí</h2>
                 </div>
-                    <p>Mi nombre es <b>Mateo Pastor González</b> y estoy cursando la asignatura <b>DWCS</b>.</p>
+                <p>Mi nombre es <b>Mateo Pastor González</b> y estoy cursando la asignatura <b>DWCS</b>.</p>
             </main>
         </div>
+
     </div>
-    <!--footer-->
-    <?php
-        include 'footer.php';
-    ?>
+    <!-- footer -->
+    <?php include 'footer.php'; ?>
 </body>
 </html>
