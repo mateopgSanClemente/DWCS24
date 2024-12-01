798fd796ab402372402c8247cc4fe2ee4164515e
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-12-01 20:45:58 +0100

    Se creó el fichero nuevoUsuario.php con el formulario para registrar un nuevo usuario.

---
+++ www/UD3/entregaTarea/nuevoUsuarioForm.php
@@ -0,0 +1,39 @@
+<?php include_once("head.php"); ?>
+    <body>
+        <!-- header -->
+        <?php include_once("header.php");?>
+          
+        <div class='container-fluid d-flex flex-column'>
+            <div class="row">
+                <?php include_once("menu.php");?>
+                <main class="col-md-9 main-content">
+                    <h2 class="pt-4 pb-2 mb-3 border-bottom">Registrar Usuario</h2>
+                    <section>
+                        <form action="nuevoUsuario.php" method="post">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" name="username" id="username" placeholder="Username del usuario" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="nombre" class="form-label">Nombre</label>
+                                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre del usuario" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="apellidos" class="form-label">Apellidos</label>
+                                <input type="text" class="form-control" name="apellidos" id="apellidos" placeholder="Apellidos del usuario" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="contrasena" class="form-label">Contraseña</label>
+                                <input type="text" class="form-control" name="contrasena" id="apellidos" placeholder="Contraseña del usuario" required>
+                            </div>
+                            <button type="submit" class="btn btn-success mb-3">Registrar</button>
+                        </form>
+                    </section>
+                </main>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <?php include_once("footer.php");?>
+    </body>
+</html>
