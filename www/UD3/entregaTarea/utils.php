ce67d6abdc0c056c6dfbbe4cb7225023b94c307c
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-12-03 01:59:51 +0100

    Se creó una función en utils.php para validar los datos introducidos en el formulario para el registro de usuario. Se creó una función en el fichero pdo.php para realizar una inserción en la tabla usuarios mediante consultas preparadas. Se creó el fichero nuevoUsuario.php para registrar el nuevos usuarios y mostrar el resultado de dicha inserción.

---
+++ www/UD3/entregaTarea/utils.php
@@ -0,0 +1,84 @@
+<?php
+    //Funciones para la tarea UD3
+    /**
+     * Función para eliminar los espacios en blanco sobrantes en los extremos de una cadena de texto, eliminar las barras invertidas y codificar caracteres especiales de HTML.
+     * @param string $input cadena de caracteres a procesar.
+     * @return string cadena de caracteres codificada.
+     */
+    function test_input ($input)
+    {
+        $input = trim ($input);
+        $input = stripcslashes ($input);
+        $input = htmlspecialchars ($input);
+        return $input;
+    }
+
+
+    /**
+     * Funcion para validar el campo.
+     * - Si el campo es alfanumérico, valida el formato.
+     * - Si el campo es numérico, se asegura que esté dentro del rango de 18 y 130, inclusive.
+     * @param string $campo valor recivido a la hora de enviar el formulario.
+     */
+    function comprobar_campo ($campo)
+    {
+        //Empiezo limpiando la entrada
+        $campo = test_input ($campo);
+
+        //Si el campo está vacio devuelvo false
+        if (empty($campo))
+        {
+            return false;
+        }
+        /* Este 'if' sobra, en este caso todos los datos del formulario se envían como tipo string.
+        if (is_string($campo))
+        {
+            
+        }
+        */
+
+        //Para los campos que pretender ser una cadena de caracteres solo se permitiran caracteres alfabéticos, seguido de un espacio opcional y otro conjunto de caracteres igual al primero, también opcional
+        //¿Realmente necesito comprobar que el campo no es un número si ya lo estoy haciendo a través de la expresión regular?
+        if (!is_numeric($campo) && preg_match("/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+( [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/", $campo))
+        {
+            return true;
+        } 
+        
+        if (is_numeric($campo))
+        {
+            //Convierto el numero que esta como un tipo string a un tipo numérico
+            //$numero = intval($campo);
+            // ^ No es necesario ya que php puede tratar la cadena como un número al utilizar operadores comparativos.
+            //Valido el valor del numero
+            if ($campo >= 18 && $campo <= 130) {
+                return true;
+            }
+        }
+
+        //En caso de no pasar ninguna de las validaciones
+        return false;
+    }
+
+    /**
+     * Valida los campos del formulario para el registro de usuarios.
+     * 
+     * @param string $campo Campo del furmulario para el registro de usuarios.
+     * @return bool  Retorna un booleano que indica el éxito de la validación
+     *               (true si se validó correctamente el campo, false en caso de error)
+     */
+    function validar_usuario ($campo)
+    {
+        //Limpio la entrada
+        $campo = test_input($campo);
+
+        if(!empty($campo) && strlen($campo) <= 100)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+?>
