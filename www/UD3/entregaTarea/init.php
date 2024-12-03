4c977fbe006d48beefb7866bb97c572d58f52fe8
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-12-03 16:31:35 +0100

    Ahora init.php se encarga de cerrar la conexión por si solo, de forma que esta acción no recae sobre las funciones que crean la base de datos y las tablas. Se hicieron cambios de estilo y en los enlaces de menu.php para que sean correctos. Se creó el fichero tareas.php para listar todas las tareas de la base de datos tareas, se incluyen en la tabla dos botones para editar y eliminar, todavía no tienen funcionalidad.

--- www/UD3/entregaTarea/init.php
+++ www/UD3/entregaTarea/init.php
@@ -12,50 +12,63 @@
                     <h2 class="pt-4 pb-2 mb-3 border-bottom">Conexión</h2>
                     <?php
                         require_once("mysqli.php");
-
-                        $conexion = conectar_mysqli("db", "root", "test", null);
-
-                        $resultado_base_datos = crear_base_datos ($conexion);
-
-                        echo "<h3 class='pt-4 pb-2 mb-3 border-bottom'>Estado de la <b>base de datos</b></h3>";
-
-                        if ($resultado_base_datos[0] === false)
+                        //Comprobar conexion
+                        try
                         {
-                            echo "<div class='alert alert-warning' role='alert'>" . $resultado_base_datos[1] . "</div>";
+                            $conexion = conectar_mysqli("db", "root", "test", null);
+                            $resultado_base_datos = crear_base_datos ($conexion);
+
+                            echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>base de datos</b></h3>";
+
+                            if ($resultado_base_datos[0] === false)
+                            {
+                                echo "<div class='alert alert-warning' role='alert'>" . $resultado_base_datos[1] . "</div>";
+                            }
+                            else if ($resultado_base_datos[0] === true)
+                            {
+                                echo "<div class='alert alert-success' role='alert'>" . $resultado_base_datos[1] . "</div>";
+                            }
+                            
+                            //Cierro la conexión
+                            cerrar_conexion($conexion);
+
+                            //Abro una nueva conexión que se conecta a la base de datos
+                            $conexion = conectar_mysqli();
+
+                            //Creo la tabla usuarios
+                            $resultado_tabla_usuarios = crear_tabla_usuario($conexion);
+                            
+                            echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla usuarios</b></h3>";
+
+                            if ($resultado_tabla_usuarios[0] === true)
+                            {
+                                echo "<div class='alert alert-success'>" . $resultado_tabla_usuarios[1] . "</div>";
+                            }
+                            elseif ($resultado_base_datos[0] === false)
+                            {
+                                echo "<div class='alert alert-warning'>" . $resultado_tabla_usuarios[1] . "</div>";
+                            }
+                            
+                            //Creo la tabla tareas
+                            $resultado_tabla_tareas = crear_tabla_tareas($conexion);
+
+                            echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla tareas</b></h3>";
+
+                            if($resultado_tabla_tareas[0] === true)
+                            {
+                                echo ("<div class='alert alert-success' role='alert'>" . $resultado_tabla_tareas[1] . "</div>");
+                            }
+                            elseif ($resultado_tabla_tareas[0] === false)
+                            {
+                                echo ("<div class='alert alert-warning' role='alert'>") . $resultado_tabla_tareas[1] . "</div>";
+                            }
+
+                            //Cierro la conexión
+                            cerrar_conexion($conexion);
                         }
-                        else if ($resultado_base_datos[0] === true)
+                        catch(Exception $e)
                         {
-                            echo "<div class='alert alert-success' role='alert'>" . $resultado_base_datos[1] . "</div>";
-                        }
-
-                        $conexion = conectar_mysqli();
-
-                        $resultado_tabla_usuarios = crear_tabla_usuario($conexion);
-                        
-                        echo "<h3 class='pt-4 pb-2 mb-3 border-bottom'>Estado de la <b>tabla usuarios</b></h3>";
-
-                        if ($resultado_tabla_usuarios[0] === true)
-                        {
-                            echo "<div class='alert alert-success'>" . $resultado_tabla_usuarios[1] . "</div>";
-                        }
-                        elseif ($resultado_base_datos[0] === false)
-                        {
-                            echo "<div class='alert alert-warning'>" . $resultado_tabla_usuarios[1] . "</div>";
-                        }
-
-                        $conexion = conectar_mysqli();
-
-                        $resultado_tabla_tareas = crear_tabla_tareas($conexion);
-
-                        echo "<h3 class='pt-4 pb-2 mb-3 border-bottom'>Estado de la <b>tabla tareas</b></h3>";
-
-                        if($resultado_tabla_tareas[0] === true)
-                        {
-                            echo ("<div class='alert alert-success' role='alert'>" . $resultado_tabla_tareas[1] . "</div>");
-                        }
-                        elseif ($resultado_tabla_tareas[0] === false)
-                        {
-                            echo ("<div class='alert alert-warning' role='alert'>") . $resultado_tabla_tareas[1] . "</div>";
+                            echo "<div class='alert alert-warning' role='alert'>Error: " . $e . "</div>";
                         }
                     ?>
             </main>
---
+++ www/UD3/entregaTarea/tareas.php
@@ -0,0 +1,73 @@
+<?php include_once("head.php"); ?>
+<body>
+    <!-- header -->
+    <?php include_once("header.php"); ?>
+    <div class="container-fluid d-flex flex-column">
+        <div class="row">
+            <!-- menu -->
+            <?php include_once("menu.php"); ?>
+            <main class="col-md-9 main-content">
+                <h2 class="border-bottom pt-4 pb-2 mb-3">Lista de Tareas</h2>
+                <?php
+                    try
+                    {
+                        require_once("mysqli.php");
+
+                        //Crear la conexion
+                        $mysqli_conn = conectar_mysqli();
+
+                        //Realizar consulta
+                        /*
+                        *   TODO: En tamaños pequeños de pantalla la tabla sobre sale por el eje horizontal,
+                        *   habría que ajustar el tamaño de la fuente para que esto no pase.
+                        */
+                        list($comprobacion, $resultado) = seleccionar_tareas($mysqli_conn);
+
+                        if(!$comprobacion)
+                        {
+                            echo "<div class='alert alert-warning'>" . $resultado . "</div>";
+                        }
+                        else
+                        {
+                            echo "<table class='table table-striped table-hover'>
+                                <thead class='table-dark'>
+                                    <tr>
+                                        <th scope='col'>ID Tarea</th>
+                                        <th scope='col'>Titulo</th>
+                                        <th scope='col'>Descripción</th>
+                                        <th scope='col'>Estado</th>
+                                        <th scope='col'>ID Usuario</th>
+                                        <th scope='col'>Acciones</th>
+                                    </tr>
+                                </thead>
+                                <tbody>";
+                            foreach($resultado as $tarea)
+                            {
+                                echo "<tr>";
+                                foreach($tarea as $datos_tarea)
+                                {
+                                    echo "<td>" . $datos_tarea . "</td>";
+                                }
+                                echo "<td>";
+                                echo "<a href='editaTareaForm.php?id=" . $tarea['id'] . "' class='btn btn-success btn-sm me-2'>Editar</a>";
+                                echo "<a href='borraTarea.php?id=" . $tarea['id'] . "' class='btn btn-danger btn-sm me-2'>Eliminar</a>";
+                                echo "</tr>";
+                            }
+                            echo "</tbody></table>";
+                        }
+                    }
+                    catch (Exception $e)
+                    {
+                        echo "<div class='alert alert-warning' role='alert'>" . "Error: " . $e->getMessage();
+                    }
+                    finally
+                    {
+                        cerrar_conexion($mysqli_conn);
+                    }
+                ?>
+            </main>
+        </div>
+    </div>
+    <!-- footer -->
+    <?php include_once("footer.php"); ?>
+</body>
