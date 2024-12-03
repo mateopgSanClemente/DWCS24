ea9f39ec2a6e590c376e94c7b4c42498d02b347e
Mateo Pastor <a21mateopg@iessanclemente.net>
2024-12-03 23:17:38 +0100

    Se eliminó el fichero style.css y su enlace en head.php.

--- www/UD3/entregaTarea/head.php
+++ www/UD3/entregaTarea/head.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UD3. Tarea</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet" type="text/css">
     <!-- Conseguí ajustar el footer de esta forma pero me gustaría hacerlo mediante clases bootstrap -->
     <style>
         footer {
