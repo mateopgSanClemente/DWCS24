8100d0948a2ec6513102f386a4660a1d34f61729
Mateo Pastor <a21mateopg@iessanclemente.net>
2025-01-14 22:09:03 +0100

    Crear la página principal.

---
+++ www/UD3/mis_donaciones/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<!-- HEAD -->
+<?php
+    include_once "head.php";
+?>
+<body>
+    <!-- HEADER -->
+    <header class="custom-header text-white text-center py-3">
+        <h2>UD3 - PÁGINA DE DONACIONES</h2>
+    </header> 
+    <div class="container-fluid d-flex flex-column">
+        <div class="row">
+            <!-- MENU -->
+            <?php
+                include_once "menu.php";
+            ?>
+            <!-- MAIN -->
+            <main class="col-md-9 main-content">
+                <div class="pt-4 pb-2 mb-3 border-bottom">
+                    <h2>Sobre mí</h2>
+                </div>
+                <p>Mi nombre es <b>Mateo Pastor González</b> y estoy cursando la asignatura <b>DWCS</b>.</p>
+            </main>
+        </div>
+    </div>
+    <!-- FOOTER -->
+    <?php
+        include_once "footer.php";
+    ?>
+</body>
+</html>
