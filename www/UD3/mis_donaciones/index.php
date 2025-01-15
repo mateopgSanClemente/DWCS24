109cb6e47d90b98477b9acd3bb7f8be772ba83f1
Mateo Pastor <a21mateopg@iessanclemente.net>
2025-01-15 22:15:17 +0100

    Cambios menores

--- www/UD3/mis_donaciones/index.php
+++ www/UD3/mis_donaciones/index.php
@@ -6,9 +6,7 @@
 ?>
 <body>
     <!-- HEADER -->
-    <header class="custom-header text-white text-center py-3">
-        <h2>UD3 - PÁGINA DE DONACIONES</h2>
-    </header> 
+    <?php include_once "header.php"; ?>
     <div class="container-fluid d-flex flex-column">
         <div class="row">
             <!-- MENU -->
@@ -21,6 +19,17 @@
                     <h2>Sobre mí</h2>
                 </div>
                 <p>Mi nombre es <b>Mateo Pastor González</b> y estoy cursando la asignatura <b>DWCS</b>.</p>
+                <div class="pt-4 pb-2 mb-3 border-bottom">
+                    <h2>Variables de entorno</h2>
+                </div>
+                <p><?php echo $_ENV["MYSQL_USER"];?></p>
+                <p><?php echo $_ENV["MYSQL_PASSWORD"];?></p>
+                <p><?php echo $_ENV["MYSQL_ROOT_PASSWORD"];?></p>
+                
+                <?php
+                    include_once ("pdo.php");
+                    $conexion_PDO = conexion_PDO();
+                ?>
             </main>
         </div>
     </div>
--- www/UD3/mis_donaciones/menu.php
+++ www/UD3/mis_donaciones/menu.php
@@ -1,8 +1,12 @@
-<aside class="custom-aside col-md-3 col-lg-3 p-3 m-2">
-    <div class="container text-center pt-3 pb-2 mb-3 border-bottom">
-        <h2>Menú Lateral</h2>
-    </div>
-    <ul class="nav flex-column">
-        <li class="list-group-item"><a class="nav-link" href="index.html">Página principal</a></li>
-    </ul>
-</aside>
+<!-- TODO: - El menú no encaja bien con la pantalla a partir de t.maños medianos de pantalla
+           - No puedo aumentar el margen izquierdo para que el menú quede separado de la pantalla -->
+<div class="container m-1 col-md-3 col-lg-3">
+    <aside class="custom-aside p-2">    
+        <div class="container text-center pt-3 pb-2 mb-3 border-bottom">
+            <h2>Menú Lateral</h2>
+        </div>
+        <ul class="nav flex-column">
+            <li class="list-group-item"><a class="nav-link" href="index.html">Página principal</a></li>
+        </ul>
+    </aside>
+</div>
