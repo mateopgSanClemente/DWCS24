c08a2d1f88fa24e4260294045d588c146e78dc17
Mateo Pastor <mateopastorg@gmail.com>
2025-02-05 22:40:26 +0100

    Implementar las funciones refactorizadas del fichero pdo.php.

--- www/UD4/entregaTarea/nuevoUsuario.php
+++ www/UD4/entregaTarea/nuevoUsuario.php
@@ -1,64 +1,60 @@
-<?php include_once ("head.php");?>
+<?php include_once "head.php";?>
     <body>
         <!-- header -->
-        <?php include_once("header.php");?>
+        <?php include_once "header.php";?>
         <div class="container-fluid d-flex flex-column">
             <div class="row">
                 <!-- menu -->
-                <?php include_once("menu.php");?>
+                <?php include_once "menu.php";?>
                 <main class="col-md-9 main-content">
                     <h2 class="pt-4 pb-2 mb-3 border-bottom">Estado del registro de usuario</h2>
                     <?php
-                    try
-                    {
-                        require_once("utils.php");
+                    try {   
+                        /**
+                         * TODO:
+                         *  - Recoger, validar y sanear si fuera necesario el nuevo campo 'rol'.
+                         */
+                        require_once "utils.php";
                         //Recoger los resultados en variables
                         $username = $_POST["username"];
                         $nombre = $_POST["nombre"];
                         $apellidos = $_POST["apellidos"];
                         $contrasena = $_POST["contrasena"];
-
                         //Comprobar errores
-                        $error = false;
-                        $mensaje_error = [];
-                        //Validar los resultados
-                        //TODO: Validar correctamente los usuarios!! Los mensajes no se imprimen correctamente
-                        list($error, $mensaje_error) = validar_usuario($username, $nombre, $apellidos, $contrasena);
-
-                        if($error)
-                        {
-                            echo "<div class='alert alert-warning'>" . $mensaje_error . "</div>";
-                        }
-                        else
-                        {
-                            require_once("pdo.php");
+                        $resultado_validar = validar_usuario($username, $nombre, $apellidos, $contrasena);
+                        //Comprobar los resultados, aunque pienso que sería más conveniente hacerlo en la página del propio formulario
+                        if (!$resultado_validar["success"]){
+                            // Creare una lista dinámica con mensajes con información sobre los errores asociados a un campo.
+                            foreach ($resultado_validar["errores"] as $nombre_campo => $errores) {
+                                echo "<h4 class='pt-2 pb-4 mb-3 border-bottom'>Campo $nombre_campo</b></h4>";
+                                echo "<ul>";
+                                foreach ($errores as $error) {                                
+                                    echo "<li class='alert alert-warning' role='alert'>" . $error . "</li>";                                     
+                                }
+                                echo "</ul>";
+                            }               
+                        } else {
+                            require_once "pdo.php";
                             //Filtrar los resultados
                             $username = test_input($username);
                             $nombre = test_input($nombre);
                             $apellidos = test_input($apellidos);
                             $contrasena = test_input($contrasena);
-
-                            //Insertar los resultados en la tabla usuarios
-
                             //Crear conexión con la base de datos
-                            list($conexion, $mensaje_estado_conexion) = conectar_PDO();
-
-                            if($conexion === false)
-                            {
-                                echo "<div class='alert alert-warning>" . $mensaje_estado_conexion . "</div>";
-                            }
-                            else
-                            {
-                                //Realizar la consulta y comprobar que esta se realizo correctamente.
-                                list($comprobacion, $resultado) = agregar_usuario($conexion, $username, $nombre, $apellidos, $contrasena);
-
-                                if($comprobacion)
-                                {
-                                    echo ("<div class='alert alert-success'>" . $resultado . "</div>");
+                            $resultado_conexion_PDO = conectar_PDO();
+                            // Variable que guarda la instancia PDO
+                            $conexion_PDO = $resultado_conexion_PDO["conexion"];
+                            if(!$resultado_conexion_PDO["success"]) {
+                                echo "<div class='alert alert-danger>" . $resultado_conexion_PDO["mensaje"] . "</div>";
+                            } else {
+                                // Insertar los datos en la tabla usuarios
+                                $resultado_agregar_usuario = agregar_usuario($conexion_PDO, $username, $nombre, $apellidos, $contrasena);
+                                // Comprobar que el usuario se agrego correctamente
+                                if(!$resultado_agregar_usuario["success"]) {
+                                    echo ("<div class='alert alert-warning'>" . $resultado_agregar_usuario["mensaje"] . "</div>");
                                 }
-                                else
-                                {
-                                    echo ("<div class='alert alert-warning'>" . $resultado . "</div>");
+                                else {
+                                    echo ("<div class='alert alert-success'>" . $resultado_agregar_usuario["mensaje"] . "</div>");
                                 }
                             }
                         }
@@ -75,6 +71,6 @@
                 </main>
             </div>
         </div>
-        <?php include_once("footer.php");?>
+        <?php include_once "footer.php";?>
     </body>
 </html>
