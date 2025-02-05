fa1cba62b41b075e8ad36d99bb1b19b7d2a5225f
Mateo Pastor <mateopastorg@gmail.com>
2025-02-05 18:04:28 +0100

    Modificar el fichero init.php para que funcione acorder a las nuevas funciones del fichero mysqli.php y devuelva un resultado coherente.

--- www/UD4/entregaTarea/init.php
+++ www/UD4/entregaTarea/init.php
@@ -1,75 +1,67 @@
-<?php include_once ("head.php"); ?>
+<?php include_once "head.php"; ?>
 <body>
     <!--header-->
-    <?php include_once ('header.php');?>
+    <?php include_once 'header.php';?>
     <div class="container-fluid d-flex flex-column">
         <div class="row">
             <!--menu-->
             <?php
-                include_once ('menu.php');
+                include_once 'menu.php';
             ?>
             <main class="col-md-9 col-sm-12 main-content">
                     <h2 class="pt-4 pb-2 mb-3 border-bottom">Conexión</h2>
                     <?php
-                        require_once("mysqli.php");
+                        require_once "mysqli.php";
                         //Comprobar conexion
-                        try
-                        {
-                            $conexion = conectar_mysqli("db", "root", "test", null);
-                            $resultado_base_datos = crear_base_datos ($conexion);
-
-                            echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>base de datos</b></h3>";
-
-                            if ($resultado_base_datos[0] === false)
-                            {
-                                echo "<div class='alert alert-warning' role='alert'>" . $resultado_base_datos[1] . "</div>";
-                            }
-                            else if ($resultado_base_datos[0] === true)
-                            {
-                                echo "<div class='alert alert-success' role='alert'>" . $resultado_base_datos[1] . "</div>";
-                            }
-                            
-                            //Cierro la conexión
-                            cerrar_conexion($conexion);
-
-                            //Abro una nueva conexión que se conecta a la base de datos
-                            $conexion = conectar_mysqli();
-
-                            //Creo la tabla usuarios
-                            $resultado_tabla_usuarios = crear_tabla_usuario($conexion);
-                            
-                            echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla usuarios</b></h3>";
-
-                            if ($resultado_tabla_usuarios[0] === true)
-                            {
-                                echo "<div class='alert alert-success'>" . $resultado_tabla_usuarios[1] . "</div>";
-                            }
-                            elseif ($resultado_base_datos[0] === false)
-                            {
-                                echo "<div class='alert alert-warning'>" . $resultado_tabla_usuarios[1] . "</div>";
-                            }
-                            
-                            //Creo la tabla tareas
-                            $resultado_tabla_tareas = crear_tabla_tareas($conexion);
-
-                            echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla tareas</b></h3>";
-
-                            if($resultado_tabla_tareas[0] === true)
-                            {
-                                echo ("<div class='alert alert-success' role='alert'>" . $resultado_tabla_tareas[1] . "</div>");
-                            }
-                            elseif ($resultado_tabla_tareas[0] === false)
-                            {
-                                echo ("<div class='alert alert-warning' role='alert'>") . $resultado_tabla_tareas[1] . "</div>";
-                            }
-
-                            //Cierro la conexión
-                            cerrar_conexion($conexion);
-                        }
-                        catch(Exception $e)
-                        {
-                            echo "<div class='alert alert-warning' role='alert'>Error: " . $e . "</div>";
-                        }
+                                              
+                        $resultado_conectar = conectar_mysqli();
+                        // Comprobar el estado de la conexión
+                        if (!$resultado_conectar["success"]){
+                            // Mensaje informativo de erro
+                            echo "<div class='alert alert-danger' role=alert'>Error en la conexión: " . $resultado_conectar["error"] . "</div>";
+                        } else {
+                            // Crear base de datos
+                            $resultado_base_datos = crear_base_datos ($resultado_conectar["conexion"]);
+                            // Comprobar que ocurrió correctamente
+                            if (!$resultado_base_datos["success"]){
+                                // Mostrar mensaje error
+                                echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>base de datos</b></h3>";
+                                echo "<div class='alert alert-danger' role=alert'>Error al crear la base de datos: " . $resultado_base_datos["mensaje"] . "</div>";
+                                // Cerrar conexión
+                                cerrar_conexion($resultado_conectar["conexion"]);
+                            } else {
+                                // Mostrar mensajse éxito.
+                                echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>base de datos</b></h3>";
+                                echo "<div class='alert alert-success' role=alert'>" . $resultado_base_datos["mensaje"] . "</div>";
+                                // Crear tabla usuarios
+                                $resultado_tabla_usuarios = crear_tabla_usuario($resultado_conectar["conexion"]);
+                                // Comprobar que se creó correctmente
+                                if (!$resultado_tabla_usuarios["success"]){
+                                    //Mostrar mensaje de error
+                                    echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla usuarios</b></h3>";
+                                    echo "<div class='alert alert-danger' role='alert'>" . $resultado_tabla_usuarios["mensaje"] . "</div>";
+                                    cerrar_conexion($resultado_conectar["conexion"]);
+                                } else {
+                                    // Mostrar mensaje de éxito
+                                    echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla usuarios</b></h3>";
+                                    echo "<div class='alert alert-success' role='alert'>" . $resultado_tabla_usuarios["mensaje"] . "</div>";
+                                    // Crear tabla tareas
+                                    $resultado_tabla_tareas = crear_tabla_tareas($resultado_conectar["conexion"]);
+                                    // Comprobar que se creó correctamente
+                                    if (!$resultado_tabla_tareas["success"]){
+                                        // Mostrar mensaje de error
+                                        echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla tareas</b></h3>";
+                                        echo "<div class='alert alert-danger' role='alert'>" . $resultado_tabla_tareas["mensaje"] . "</div>";                                            
+                                        cerrar_conexion($resultado_conectar["conexion"]);
+                                    } else {
+                                        // Mostrar mensaje de éxito
+                                        echo "<h3 class='pt-2 pb-4 mb-3 border-bottom'>Estado de la <b>tabla tareas</b></h3>";
+                                        echo "<div class='alert alert-success' role='alert'>" . $resultado_tabla_tareas["mensaje"] . "</div>";
+                                        cerrar_conexion($resultado_conectar["conexion"]);
+                                    }
+                                }
+                            }                        
+                        }        
                     ?>
             </main>
         </div>
