0cb47c5287661fb876a8d87c56a8bdedb78a2f0e
Mateo Pastor <mateopastorg@gmail.com>
2025-02-06 18:43:01 +0100

    Se modificó el fichero borraUsuario.php para usar correctamente las nuevas funciones.

--- www/UD4/entregaTarea/borraUsuario.php
+++ www/UD4/entregaTarea/borraUsuario.php
@@ -1,4 +1,4 @@
-<?php include_once ("head.php");?>
+<?php include_once "head.php";?>
     <body>
         <!-- header -->
         <?php include_once("header.php");?>
@@ -10,32 +10,31 @@
                     <h2 class="pt-4 pb-2 mb-3 border-bottom">Estado de la eliminación de usuario</h2>
                     <?php
                         //Crear y comprobar conecxion
-                        require_once ("pdo.php");
-                        
-                        list($conexion, $mensaje_estado_conexion) = conectar_PDO();
-
-                        if($conexion === false)
-                        {
-                            echo "<div class='alert alert-warning>" . $mensaje_estado_conexion . "</div>";
-                        }
-                        else
-                        {
-                            $id = $_GET['id'];
-                            list($comprobacion, $resultado) = eliminar_usuario($conexion, $id);
-
-                            if(!$comprobacion)
-                            {
-                                echo ("<div class='alert alert-warning' role='alert'>" . $resultado . "</div>");
+                        require_once "pdo.php";
+                        // Crear conexión PDO
+                        $resultado_conexion_PDO = conectar_PDO();
+                        // Comprobar que la conexión fue exitosa
+                        if(!$resultado_conexion_PDO["success"]){
+                            echo "<div class='alert alert-danger' role='alert'>" . $resultado_conexion_PDO["error"] . "</div>";
+                        } else {
+                            // Guardar la conexión en una variable
+                            $conexion_PDO = $resultado_conexion_PDO["conexion"];
+                            $id_usuario = $_GET["id"];
+                            // Eliminar usuario
+                            $resultado_eliminar_usuario = eliminar_usuario($conexion_PDO, $id_usuario);
+                            // Mostrar resultado
+                            if (!$resultado_eliminar_usuario["success"]){
+                                echo "<div class='alert alert-warning' role='alert'>" . $resultado_eliminar_usuario["mensaje"] . "</div>";
+                            } else {
+                                echo "<div class='alert alert-success' role='alert'>" . $resultado_eliminar_usuario["mensaje"] . "</div>";
                             }
-                            else
-                            {
-                                echo ("<div class='alert alert-success' role='alert'>" . $resultado . "</div>");
-                            }
+                            // Cerrar conexión
+                            $conexion_PDO = null;
                         }
                     ?>
                 </main>
             </div>
         </div>
-        <?php include_once("footer.php");?>
+        <?php include_once "footer.php";?>
     </body>
 </html>
