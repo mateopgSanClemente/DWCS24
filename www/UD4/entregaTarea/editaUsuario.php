79e6e21761d9bcdc76dc600076879d44b60cf6c2
Mateo Pastor <mateopastorg@gmail.com>
2025-02-06 23:48:29 +0100

    Se modificó el fichero editarUsuario.php para que funcione con las nuevas funciones.

--- www/UD4/entregaTarea/editaUsuario.php
+++ www/UD4/entregaTarea/editaUsuario.php
@@ -1,122 +1,66 @@
-<?php include_once ("head.php");?>
+<?php include_once "head.php";?>
     <body>
         <!-- header -->
-        <?php include_once("header.php");?>
+        <?php include_once "header.php";?>
         <div class="container-fluid d-flex flex-column">
             <div class="row">
                 <!-- menu -->
-                <?php include_once("menu.php");?>
+                <?php include_once "menu.php";?>
                 <main class="col-md-9 main-content">
                     <h2 class="pt-4 pb-2 mb-3 border-bottom">Estado de la modificación de usuario</h2>
-                    <?php
-                        require_once("utils.php");
-
-                        require_once ("pdo.php");
-                        
-                        list($conexion, $mensaje_estado_conexion) = conectar_PDO();
-
-                        if($conexion === false)
-                        {
-                            echo "<div class='alert alert-warning>" . $mensaje_estado_conexion . "</div>";
-                        }
-                        else
-                        {
-                            $id = $_GET['id'];
-                            list($comprobacion, $resultado) = seleccionar_usuario_id($conexion, $id);
-
-                            if(!$comprobacion)
-                            {
-                                echo ("<div class='alert alert-warning' role='alert'>" . $resultado . "</div>");
-                            }
-                            else
-                            {
-                                //Recuperar los datos originales del usuario
-                                $usuario_original = [
-                                    'username' => htmlspecialchars_decode($resultado['username']),
-                                    'nombre' => htmlspecialchars_decode($resultado['nombre']),
-                                    'apellidos' => htmlspecialchars_decode($resultado['apellidos']),
-                                    'contrasena' => htmlspecialchars_decode($resultado['contrasena'])
-                                ];
-
-                                //Comprobar errores
-                                $error = false;
-                                $mensaje_error = [];
-
-                                //Validar los resultados
-                                if(!empty($username) && !validar_usuario($username))
-                                {
-                                    $error = true;
-                                    $mensaje_error[] = "El campo 'Username' debe contener un máximo de 50 caracteres.";
+                    <?php   
+                        require_once "utils.php";
+                        // Guardar ID usuario en una variable
+                        $id_usuario = $_GET['id'];
+                        // Recuperar los datos enviados a través del formulario
+                        $usuario_username_nuevo = $_POST["username"];
+                        $usuario_nombre_nuevo = $_POST["nombre"];
+                        $usuario_apellidos_nuevo = $_POST["apellidos"];
+                        $usuario_contrasena_nuevo = $_POST["contrasena"];
+                        // Validar los datos, si no son válidos, mostrar mensaje de error, todos son obligatorios menos la contraseña
+                        $resultado_validar_usuario = validar_modificar_usuario($usuario_username_nuevo, $usuario_nombre_nuevo, $usuario_apellidos_nuevo, $usuario_contrasena_nuevo);
+                        //Comprobar los resultados, aunque pienso que sería más conveniente hacerlo en la página del propio formulario
+                        if (!$resultado_validar_usuario["success"]){
+                            // Crear una lista dinámica de mensajes con información sobre los errores asociados a un campo.
+                            foreach ($resultado_validar["errores"] as $nombre_campo => $errores) {
+                                echo "<h4 class='pt-2 pb-4 mb-3 border-bottom'>Campo <b>$nombre_campo</b></h4>";
+                                echo "<ul>";
+                                foreach ($errores as $error) {                                
+                                    echo "<li class='alert alert-warning' role='alert'>" . $error . "</li>";                                     
                                 }
-
-                                if(!empty($nombre) && !validar_usuario($nombre))
-                                {
-                                    $error = true;
-                                    $mensaje_error[] = "El campo 'Nombre' debe contener un máximo de 50 caracteres.";
+                                echo "</ul>";
+                            }               
+                        } else {
+                            // Si los resultados son válidos, sanearlos
+                            $usuario_username_nuevo = test_input($usuario_username_nuevo);
+                            $usuario_nombre_nuevo = test_input($usuario_nombre_nuevo);
+                            $usuario_apellidos_nuevo = test_input($usuario_apellidos_nuevo);
+                            $usuario_contrasena_nuevo = test_input($usuario_contrasena_nuevo);
+                            // Crear una conexión PDO
+                            require_once "pdo.php";
+                            $resultado_conexión_PDO = conectar_PDO();
+                            // Comprobar la conexión
+                            if (!$resultado_conexión_PDO["success"]){
+                                echo "<div class='alert alert-danger'>" . $resultado_conexión_PDO["mensaje"] . "</div>";
+                            } else {
+                                // Guardar conexión PDO en una variable
+                                $conexion_PDO = $resultado_conexión_PDO["conexion"];
+                                // Modificar usuario
+                                $resultado_modificar_usuario = modificar_usuario($conexion_PDO, $id_usuario, $usuario_username_nuevo, $usuario_nombre_nuevo, $usuario_apellidos_nuevo, $usuario_contrasena_nuevo);
+                                //Mostrar mensaje con los resultados de la modificación
+                                if (!$resultado_modificar_usuario["success"]){
+                                    echo "<div class='alert alert-warning'>" . $resultado_modificar_usuario["mensaje"] . "</div>";
+                                } else {
+                                    echo "<div class='alert alert-success'>" . $resultado_modificar_usuario["mensaje"] . "</div>";
                                 }
-                                if(!empty($apellidos) && !validar_usuario($apellidos))
-                                {
-                                    $error = true;
-                                    $mensaje_error[] = "El campo 'Apellidos' debe contener un máximo de 100 caracteres.";
-                                }
-
-                                //Compruebo que el campo contraseña no esté vacío antes de validarlo porque quiero que se conserve la orginal en caso de que esté vacía.
-                                if(!empty($contrasena) && !validar_usuario($contrasena))
-                                {
-                                    $error = true;
-                                    $mensaje_error[] = "El campo 'Contraseña' debe contener un máximo de 100 caracteres.";
-                                }
-                                //En caso de que no se den errores
-                                if(!$error)
-                                {
-                                    //Recoger los resultados en variables
-                                    //TODO: CREO QUE NO HACE FALTA
-                                    $username = $_POST["username"];
-                                    $nombre = $_POST["nombre"];
-                                    $apellidos = $_POST["apellidos"];
-                                    $contrasena = $_POST["contrasena"];
-                                    //Filtrar los resultados
-                                    $username = test_input($username);
-                                    $nombre = test_input($nombre);
-                                    $apellidos = test_input($apellidos);
-                                    $contrasena = test_input($contrasena);
-
-                                    //Si lo campos introducidos están vacíos se compruebas los valores originales
-                                    //TODO: tiene que haber forma de hacer esto mediante un bucle
-                                    if (empty($username))
-                                    {
-                                        $username = $usuario_original['username'];
-                                    }
-                                    if (empty($nombre))
-                                    {
-                                        $nombre = $usuario_original['nombre'];
-                                    }
-                                    if (empty($apellidos))
-                                    {
-                                        $apellidos = $usuario_original['apellidos'];
-                                    }
-                                    if (empty($contrasena))
-                                    {
-                                        $contrasena = $usuario_original['contrasena'];
-                                    }
-
-                                    //Realizar la consulta y comprobar que esta se realizo correctamente.
-                                    list($comprobacion, $resultado) = modificar_usuario($conexion, $id, $username, $nombre, $apellidos, $contrasena);
-                                    if($comprobacion)
-                                    {
-                                        echo ("<div class='alert alert-success'>" . $resultado . "</div>");
-                                    }
-                                    else
-                                    {
-                                        echo ("<div class='alert alert-warning'>" . $resultado . "</div>");
-                                    }
-                                }
+                                // Cerrar conexión
+                                $conexion_PDO = null;
                             }
                         }
                     ?>
                 </main>
             </div>
         </div>
-        <?php include_once("footer.php");?>
+        <?php include_once "footer.php";?>
     </body>
 </html>
