83a1f41b56cbe418ce6b679a7bb4d2a11e4c92a2
Mateo Pastor <mateopastorg@gmail.com>
2025-02-06 17:41:34 +0100

    Se modificó el fichero para que utilice correctamente las funciones refactorizadas de los ficheros pdo.php y mysqli.php.

--- www/UD4/entregaTarea/tareas.php
+++ www/UD4/entregaTarea/tareas.php
@@ -1,101 +1,92 @@
-<?php include_once("head.php"); ?>
+<?php include_once "head.php"; ?>
 <body>
     <!-- header -->
-    <?php include_once("header.php"); ?>
+    <?php include_once "header.php"; ?>
     <div class="container-fluid d-flex flex-column">
         <div class="row">
             <!-- menu -->
-            <?php include_once("menu.php"); ?>
+            <?php include_once "menu.php"; ?>
             <main class="col-md-9 main-content">
                 <h2 class="border-bottom pt-4 pb-2 mb-3">Lista de Tareas</h2>
                 <?php
-                if(empty($_POST))
-                {
-                    try
-                    {
-                        require_once("mysqli.php");
-
-                        //Crear la conexion
-                        $mysqli_conn = conectar_mysqli();
-
-                        //Realizar consulta
-                        /*
-                        *   TODO: En tamaños pequeños de pantalla la tabla sobre sale por el eje horizontal,
-                        *   habría que ajustar el tamaño de la fuente para que esto no pase.
-                        */
-                        list($comprobacion, $resultado) = seleccionar_tareas($mysqli_conn);
-
-                        if(!$comprobacion)
-                        {
-                            echo "<div class='alert alert-warning'>" . $resultado . "</div>";
-                        }
-                        else
-                        {
+                /**
+                 *  TODO:
+                 *  - Adaptar el fichero a las nuevas fuciones de pdo y mysqli.
+                 */
+                if(empty($_POST)) {
+                    require_once "mysqli.php";
+                    // Crear la conexion
+                    $resultado_conexion_mysqli = conectar_mysqli();
+                    // Comprobar que la conexión fue exitosa
+                    if (!$resultado_conexion_mysqli["success"]){
+                        // Mostrar mensaje de error
+                        echo "<div class='alert alert-danger' role='alert'>" . $resultado_conexion_mysqli["error"] . "</div>";
+                    } else {
+                        // Si la conexión fue exitosa, guardar la conexión en una variable por comodidad
+                        $conexion_mysqli = $resultado_conexion_mysqli["conexion"];
+                        // Seleccionar tarea
+                        $resultado_seleccionar_tareas = seleccionar_tareas($conexion_mysqli);
+                        //Comprobar que las tareas se seleccionaron correctamente
+                        if (!$resultado_seleccionar_tareas["success"]){
+                            // Mostrar mensaje de error
+                            echo "<div class='alert alert-warning' role='alert'>" . $resultado_seleccionar_tareas["mensaje"] . "</div>";
+                        } else {
+                            // Recuperar tareas y mostrar en una tabla sus características y su usuario
+                            $tareas = $resultado_seleccionar_tareas["datos"];
                             echo "<table class='table table-striped table-hover'>
-                                <thead class='table-dark'>
-                                    <tr>
-                                        <th scope='col'>ID Tarea</th>
-                                        <th scope='col'>Titulo</th>
-                                        <th scope='col'>Descripción</th>
-                                        <th scope='col'>Estado</th>
-                                        <th scope='col'>Username</th>
-                                        <th scope='col'>Acciones</th>
-                                    </tr>
-                                </thead>
-                                <tbody>";
-                            foreach($resultado as $tarea)
-                            {
-                                echo "<tr>";
-                                foreach($tarea as $datos_tarea)
-                                {
-                                    echo "<td>" . $datos_tarea . "</td>";
-                                }
-                                echo "<td>";
-                                echo "<a href='editaTareaForm.php?id=" . $tarea['id'] . "' class='btn btn-success btn-sm me-2'>Editar</a>";
-                                echo "<a href='borraTarea.php?id=" . $tarea['id'] . "' class='btn btn-danger btn-sm me-2'>Eliminar</a>";
-                                echo "</tr>";
-                            }
-                            echo "</tbody></table>";
+                                    <thead class='table-dark'>
+                                        <tr>
+                                            <th scope='col'>ID Tarea</th>
+                                            <th scope='col'>Titulo</th>
+                                            <th scope='col'>Descripción</th>
+                                            <th scope='col'>Estado</th>
+                                            <th scope='col'>Username</th>
+                                            <th scope='col'>Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>";
+                                    foreach($tareas as $tarea)
+                                    {
+                                        echo "<tr>";
+                                        foreach($tarea as $datos_tarea)
+                                        {
+                                            echo "<td>" . $datos_tarea . "</td>";
+                                        }
+                                        echo "<td>";
+                                        echo "<a href='editaTareaForm.php?id=" . $tarea['id'] . "' class='btn btn-success btn-sm me-2'>Editar</a>";
+                                        echo "<a href='borraTarea.php?id=" . $tarea['id'] . "' class='btn btn-danger btn-sm me-2'>Eliminar</a>";
+                                        echo "</td>";
+                                        echo "</tr>";
+                                    }
+                                    echo "</tbody></table>";
                         }
+                        // Cerrar conexión
+                        cerrar_conexion($conexion_mysqli);
                     }
-                    catch (Exception $e)
-                    {
-                        echo "<div class='alert alert-warning' role='alert'>" . "Error: " . $e->getMessage();
-                    }
-                    finally
-                    {
-                        cerrar_conexion($mysqli_conn);
-                    }
-                }
-                else
-                {
-                    try
-                    {
-                        require_once("pdo.php");
-
-                        //Crear la conexion
-                        list($PDO_conn, $mensaje_estado_conexion) = conectar_PDO();
-                        //TODO: comprobar conexión
-
-                        //Variables de id de usuario y estado de la tarea
+                } else {
+                    require_once "pdo.php";
+                    // Crear la conexion
+                    $resultado_conexion_PDO = conectar_PDO();
+                    // Comprobar que la cnexión fue exitosa
+                    if (!$resultado_conexion_PDO["success"]){
+                        echo "<div class='alert alert-danger' role='alert'>" . $resultado_conexion_mysqli["mensaje"] . "</div>";
+                    } else {
+                        // Guardar la conexión en una variable
+                        $conexion_PDO = $resultado_conexion_PDO["conexion"];
+                        // ID usuario para mostrar tareas asociada.
                         $id_usuario = $_POST["username"];
-                        $comprobacion;
-                        $resultado;
-                        $estado_tarea;
-                        list($comprobacion, $resultado) = seleccionar_tarea_username_estado($PDO_conn, $id_usuario);
-                        if(isset($_POST["estado"]))
-                        {
+                        // En caso de que se especifique el estado de la tarea
+                        if(isset($_POST["estado"])) {
                             $estado_tarea = $_POST["estado"];
-                            list($comprobacion, $resultado) = seleccionar_tarea_username_estado($PDO_conn, $id_usuario, $estado_tarea);
-                        }
-
-                        if(!$comprobacion)
-                        {
-                            echo "<div class='alert alert-warning'>" . $resultado . "</div>";
-                        }
-                        else
-                        {
-                            echo "<table class='table table-striped table-hover'>
+                            // Seleccionar las tareas por su id_usuario y estado
+                            $resultado_seleccionar_tareas = tareas_usuario_estado($conexion_PDO, $id_usuario, $estado_tarea);
+                            // Comprobar que se seleccionaron correctamente
+                            if (!$resultado_seleccionar_tareas["success"]){
+                                // Mostar mensaje
+                                echo "<div class='alert alert-warning' role='alert'>" . $resultado_seleccionar_tareas["mensaje"] . "</div>";
+                            } else {
+                                // Crear una tabla dinámica
+                                echo "<table class='table table-striped table-hover'>
                                 <thead class='table-dark'>
                                     <tr>
                                         <th scope='col'>ID Tarea</th>
@@ -107,24 +98,24 @@
                                     </tr>
                                 </thead>
                                 <tbody>";
-                            foreach($resultado as $tarea)
-                            {
-                                echo "<tr>";
-                                foreach($tarea as $datos_tarea)
+                                foreach($resultado_seleccionar_tareas["datos"] as $tarea)
                                 {
-                                    echo "<td>" . $datos_tarea . "</td>";
+                                    echo "<tr>";
+                                    foreach($tarea as $datos_tarea)
+                                    {
+                                        echo "<td>" . $datos_tarea . "</td>";
+                                    }
+                                    echo "<td>";
+                                    echo "<a href='editaTareaForm.php?id=" . $tarea['id'] . "' class='btn btn-success btn-sm me-2'>Editar</a>";
+                                    echo "<a href='borraTarea.php?id=" . $tarea['id'] . "' class='btn btn-danger btn-sm me-2'>Eliminar</a>";
+                                    echo "</td>";
+                                    echo "</tr>";
                                 }
-                                echo "<td>";
-                                echo "<a href='editaTareaForm.php?id=" . $tarea['id'] . "' class='btn btn-success btn-sm me-2'>Editar</a>";
-                                echo "<a href='borraTarea.php?id=" . $tarea['id'] . "' class='btn btn-danger btn-sm me-2'>Eliminar</a>";
-                                echo "</tr>";
+                                echo "</tbody></table>";
                             }
-                            echo "</tbody></table>";
                         }
-                    }
-                    catch (Exception $e)
-                    {
-                        echo "<div class='alert alert-warning' role='alert'>" . "Error: " . $e->getMessage();
+                        // Cerrar conexión
+                        $conexion_PDO = null;
                     }
                 }
                 ?>
