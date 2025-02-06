066a4ea3304d3547bc50163fc82614c4746bd394
Mateo Pastor <mateopastorg@gmail.com>
2025-02-07 00:59:44 +0100

    Modificar fichero para que funcione con las nuevas funciones.

--- www/UD4/entregaTarea/buscaTareas.php
+++ www/UD4/entregaTarea/buscaTareas.php
@@ -1,12 +1,12 @@
-<?php include_once("head.php"); ?>
+<?php include_once "head.php"; ?>
     <body>
         <!-- header -->
-        <?php include_once("header.php"); ?>
+        <?php include_once "header.php"; ?>
 
         <div class='container-fluid d-flex flex-column'>
             <div class="row">
                 <!-- menu -->
-                <?php include_once("menu.php");?>
+                <?php include_once "menu.php";?>
                 <main class="col-md-9 main-content">
                     <h2 class="pt-4 pb-2 mb-3 border-bottom">Buscar Tareas</h2>
                     <section>
@@ -14,34 +14,33 @@
                             <div class="mb-3">
                                 <label for="username" class="form-label">Username</label>
                                 <?php
-                                    include_once("pdo.php");
+                                    include_once "pdo.php";
                                     //Conexión PDO
-                                    list($PDO_con, $mensaje_estado_conexión) = conectar_PDO();
-                                    //Comprobar conexión PDO
-                                    if($PDO_con === false)
-                                    {
-                                        echo "<div class='alert alert-warning'>" . $mensaje_estado_conexión . "</div>";
-                                    }
-                                    else
-                                    {
+                                    $resultado_conectar_PDO = conectar_PDO();
+                                    if(!$resultado_conectar_PDO["success"]){
+                                        echo "<div class='alert alert-danger'>" . $resultado_conectar_PDO["mensaje"] . "</div>";
+                                    } else {
                                         //Seleccionar usuarios
-                                        list($comprobacion, $resultado) = seleccionar_usuarios($PDO_con);
+                                        $conexion_PDO = $resultado_conectar_PDO["conexion"];
+                                        $resultado_seleccionar = seleccionar_usuarios($conexion_PDO);
+
                                         //Comprobar que los datos se seleccionaron correctamente
-                                        if(!$comprobacion)
+                                        if(!$resultado_seleccionar["success"])
                                         {
-                                            echo "<div class='alert alert-warning'>" . $resultado . "</div>";
+                                            echo "<div class='alert alert-warning'>" . $resultado_seleccionar["datos"] . "</div>";
                                         }
                                         else
                                         {
                                             echo "<select class='form-select' name='username' id='username' required>";
                                             echo "<option value='' selected disabled>Selecciona un usuario</option>";
-                                            foreach($resultado as $usuario)
+                                            foreach($resultado_seleccionar["datos"] as $usuario)
                                             {
                                                 echo "<option value='" . $usuario['id'] . "'>" . $usuario["username"] . "</option>";
                                             }
                                             echo "</select>";
                                         }
                                     }
+
                                 ?>
                             </div>
                             <div class="mb-3">
