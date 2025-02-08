49877e659f77790df5a4177bd93069734ed708ca
Mateo Pastor <mateopastorg@gmail.com>
2025-02-08 15:20:24 +0100

    Genenrar dinamicamente el acceso a parte del menú en función de si el rol es administrador o no.

--- www/UD4/entregaTarea/menu.php
+++ www/UD4/entregaTarea/menu.php
@@ -4,12 +4,16 @@
     </div>
     <ul class="list-group">
         <li class="list-group-item"><a class="nav-link" href="index.php">Página principal</a></li>
-        <li class="list-group-item"><a class="nav-link" href="init.php">Iniciar base de datos y tabla</a></li>
-        <li class="list-group-item"><a class="nav-link" href="nuevoUsuarioForm.php">Nuevo usuario</a></li>
-        <li class="list-group-item"><a class="nav-link" href="usuarios.php">Listar usuarios</a></li>
         <li class="list-group-item"><a class="nav-link" href="nuevaForm.php">Nueva tarea</a></li>
         <li class="list-group-item"><a class="nav-link" href="tareas.php">Listar tareas</a></li>
-        <li class="list-group-item"><a class="nav-link" href="buscaTareas.php">Buscar tareas de usuario</a></li>
+        <?php
+            if ($_SESSION["rol"] === 1){
+                echo "<li class='list-group-item'><a class='nav-link' href='init.php'>Iniciar base de datos y tabla</a></li>";
+                echo "<li class='list-group-item'><a class='nav-link' href='nuevoUsuarioForm.php'>Nuevo usuario</a></li>";
+                echo "<li class='list-group-item'><a class='nav-link' href='usuarios.php'>Listar usuarios</a></li>";
+                echo "<li class='list-group-item'><a class='nav-link' href='buscaTareas.php'>Buscar tareas de usuario</a></li>"; 
+            }
+        ?>
         <li class="list-group-item"><a class="nav-link" href="logout.php">Cerrar sesión</a></li>
     </ul>
 </aside>
