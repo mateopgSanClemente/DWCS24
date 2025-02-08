043693594ef2a49e6fc207ffbe6e6cf040b8059a
Mateo Pastor <mateopastorg@gmail.com>
2025-02-08 12:47:13 +0100

    Muestra un mensaje de error en caso de que la autenticación del usuario haya tenido problemas.

--- www/UD4/entregaTarea/login.php
+++ www/UD4/entregaTarea/login.php
@@ -6,17 +6,38 @@
             <div class="card mx-auto" style="max-width: 400px;">
                 <div class="card-body">
                     <h2 class="card-title text-center mb-4">Iniciar Sesión</h2>
-                    <form action="login.php" method="post">
+                    <?php
+                        // Comprobar que no hay errores
+                        if (isset($_GET["error"])){
+                            // Aviso en caso de que no se haya iniciado la sesión
+                            if (($_GET["error"]) === "sesion") {
+                                echo "<div class='alert alert-danger' role='alert'>" . "Es necesario iniciar la sesión" . "</div>";
+                            }
+                            // Aviso en caso de que no se pudiese conectar con la base de datos
+                            if (($_GET["error"] === "conexion")) {
+                                echo "<div class='alert alert-warning' role='alert'>" . "No se pudo conectar con la base de datos" . "</div>";
+                            }
+                            // Aviso en caso de que el usuario no exista
+                            if (($_GET["error"] === "usuario")) {
+                                echo "<div class='alert alert-warning' role='alert'>" . "No se encontró el usuario en la base de datos" . "</div>";
+                            }
+                            // Aviso en caso de que la contraseña no coincida.
+                            if (($_GET["error"]) === "pass") {
+                                echo "<div class='alert alert-warning' role='alert'>" . "La contraseña no es correcta" . "</div>";
+                            }
+                        }
+                    ?>
+                    <form action="loginAuth.php" method="post">
                         <!-- Campo para el username -->
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                         <label for="username" class="form-label">Usuario</label>
-                        <input type="text" class="form-control mb-3" name="username" id="username" placeholder="Ingresa tu usuario" required>
+                        <input type="text" class="form-control" name="username" id="username" placeholder="Ingresa tu usuario" required>
                         </div>
 
                         <!-- Campo para la contraseña -->
-                        <div class="form-group">
+                        <div class="form-group mb-3">
                         <label for="password" class="form-label">Contraseña</label>
-                        <input type="password" class="form-control mb-3" name="password" id="password" placeholder="Ingresa tu contraseña" required>
+                        <input type="password" class="form-control" name="password" id="password" placeholder="Ingresa tu contraseña" required>
                         </div>
 
                         <!-- Botón de envío -->
