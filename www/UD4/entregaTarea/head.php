7ebb08034d805ed9735fc7f4869e9cecf8521a5d
Mateo Pastor <mateopastorg@gmail.com>
2025-02-09 03:22:18 +0100

    Enlazar iconos de bootstrap en el elemento head.

--- www/UD4/entregaTarea/head.php
+++ www/UD4/entregaTarea/head.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UD4. Tarea</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+
     <!-- Conseguí ajustar el footer de esta forma pero me gustaría hacerlo mediante clases bootstrap -->
     <style>
         footer {
