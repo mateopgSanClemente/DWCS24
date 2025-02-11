96c4066ad7e4d46bb513d433565f52715dd0bd72
Mateo Pastor <mateopastorg@gmail.com>
2025-02-11 15:42:02 +0100

    validar_modificar implementa el uso de la clase Usuario.

--- www/UD5/mis_soluciones/tienda/validar_modificar.php
+++ www/UD5/mis_soluciones/tienda/validar_modificar.php
@@ -1,66 +1,41 @@
 <?php
     session_start();
     require_once("utils_validar.php");
+    // Sanear los datos que provengan de formularios
+    $id = intval(test_input($_GET['id']));
+    $nombre = test_input($_POST['nombre']);
+    $apellido = test_input($_POST['apellidos']);
+    $edad = test_input($_POST['edad']);
+    $provincia = test_input($_POST['provincia']);
 
-    $id = $_GET['id'];
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellidos'];
-    $edad = $_POST['edad'];
-    $provincia = $_POST['provincia'];
+    // Crear un objeto Usuario con los datos del fomrulario
+    require_once "Usuario.php";
+    $cliente = new Usuario($nombre, $apellido, $edad, $provincia);
+    $cliente->setId($id);
+    // Validar usuario mediante el método validar del propiedo objeto.
+    $resultado_validar = $cliente->validar();
 
-    //Validar los datos
-    $error = false;
-    $mensaje_error = [];
-    //Validar nombre
-    if(!comprobar_campo($nombre))
-    {
-        $error = true;
-        $mensaje_error[] = "El campo 'nombre' no puede estar vacio y debe contener por lo menos un caracter alfabético."; 
-    }
-    //Validar apellidos
-    if(!comprobar_campo($apellido))
-    {
-        $error = true;
-        $mensaje_error[] = "El campo 'apellidos' no puede estar vacio y debe contener por lo menos un caracter alfabético.";
-    }
-    //Validar edad
-    if(!comprobar_campo($edad))
-    {
-        $error = true;
-        $mensaje_error[] = "El campo 'edad' no puede estar vacío y debe ser un número que se encuentre entre los valores 18 y 130.";
-    }
-    //Validar provincia
-    if(!comprobar_campo($provincia))
-    {
-        $error = true;
-        $mensaje_error[] = "El campo 'provincia' no puede estar vacio y debe contener por lo menos un caracter alfabético.";
-    }
-
-    if ($error) {
-        $_SESSION["error"] = $mensaje_error;
+    // Comprobar que la validación fue correcta
+    if(!$resultado_validar["success"]){
+        $_SESSION["error"] = $resultado_validar["resultado"];
         header("Location: modificar.php?id=$id&nombre=$nombre&apellido=$apellido");
         exit;
-
     } else {
 
-        require_once("utils_bases_datos.php");
-        
+        // Crear conexión mysqli
+        require_once("utils_bases_datos.php");      
         $conexion = conectar();
-        $id = test_input($id);
-        $nombre = test_input($nombre);
-        $apellido = test_input($apellido);
-        $edad = test_input($edad);
-        $provincia = test_input($provincia);          
-        $resultado = modificar_cliente($conexion, $id, $nombre, $apellido, $edad, $provincia);
+          
+        $resultado = modificar_cliente($conexion, $cliente);
 
-        if ($resultado[0] === true)
+        if ($resultado["success"])
         {
-            $_SESSION["success"] = $resultado[1];
+            $_SESSION["success"] = $resultado["mensaje"];
             header ("Location: listar.php");
         }
-        else if ($resultado[0] === false)
+        else
         {
-            $_SESSION["error"] = $resultado[1];
+            $_SESSION["error"] = $resultado["mensaje"];
             header ("Location: listar.php");
         }
         cerrar_conexion($conexion);
