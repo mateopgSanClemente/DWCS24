d3003e73673aef6972a5cf33ed6bc01aeb09c41a
Mateo Pastor <mateopastorg@gmail.com>
2025-02-14 20:48:35 +0100

    Crear clase Usuarios.

---
+++ www/UD5/entregaTarea/clases/usuarios.php
@@ -0,0 +1,64 @@
+<?php
+
+class Usuarios {
+    
+    // Propiedades
+    private ?int $id;
+    private string $username;
+    private string $nombre;
+    private string $apellidos;
+    private string $contrasena;
+    private int $rol;
+
+    // Contructor
+    public function __construct(string $username, string $nombre, string $apellidos, string $contrasena, int $rol, ?int $id = null )
+    {
+        $this->username = $username;
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->contrasena = $contrasena;
+        $this->rol = $rol;
+        $this->id = $id;
+    }
+
+    // Getters
+    public function getUsername(): string {
+        return $this->username;
+    }
+    public function getNombre(): string {
+        return $this->nombre;
+    }
+    public function getApellidos(): string {
+        return $this->apellidos;
+    }
+    public function getContrasena(): string {
+        return $this->contrasena;
+    }
+    public function getRol(): int {
+        return $this->rol;
+    }
+    public function getId(): int {
+        return $this->id;
+    }
+
+    // Setters
+    public function setUsername(string $username): void {
+        $this->username = $username;
+    }
+    public function setNombre(string $nombre): void {
+        $this->nombre = $nombre;
+    }
+    public function setApellidos(string $apellidos): void {
+        $this->apellidos = $apellidos;
+    }
+    public function setContrasena(string $contrasena): void {
+        $this->contrasena = $contrasena;
+    }
+    public function setRol(int $rol): void {
+        $this->rol = $rol;
+    }
+    public function setId(?int $id): void {
+        $this->id = $id;
+    }
+}   
+?>
